b6316d8a888e6fa3b57fb66848d0414c82a9abb0
Emmett Hasley <Ehasley1136@jh-mba158-06.d214.org>
2019-02-12 11:32:49 -0600

    added table view

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -4,17 +4,29 @@
 //
 //  Created by Emmett Hasley on 2/8/19.
 //  Copyright © 2019 John Heresy High School. All rights reserved.
-// Bingo Bongo 
-
+// Bingo Bongo
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var newItemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addNewItemButtonPressed(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
